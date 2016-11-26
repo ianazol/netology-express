fc712f8b8b558434a92f7576bc309e373c82b2b7
Iana <yana.kolchanova@gmail.com>
2016-11-26 15:52:26 +0300

    refactor rest

--- rest.js
+++ rest.js
@@ -28,40 +28,80 @@ app.post("/users", function(req, res){
 app.get("/users/:id", function(req, res){
 	let index = getUserIndex(req.params.id);
 
-	if (index < 0)
+	if (index < 0){
 		res.status(404).send('User not found');
+	} else {
+		let data = {};
 
-	res.json(users[index]);
+		if (!!req.query.fields){
+			let fields = req.query.fields.split(",");
+			fields.forEach((field) => data[field] = users[index][field]);
+		} else {
+			data = users[index];
+		}
+		res.json(data);
+	}
 });
 
 //update
 app.put("/users/:id", function(req, res){
 	let index = getUserIndex(req.params.id);
 
-	if (index < 0)
+	if (index < 0){
 		res.status(404).send('User not found');
+	} else {
+		if (req.body.name)
+			users[index].name = req.body.name;
+		if (req.body.score)
+			users[index].score = req.body.score;
 
-	if (req.body.name)
-		users[index].name = req.body.name;
-	if (req.body.score)
-		users[index].score = req.body.score;
-
-    res.json(users[index]);
+	    res.json(users[index]);
+	}
 });
 
 //delete
 app.delete("/users/:id", function(req, res){
 	let index = getUserIndex(req.params.id);
 
-	if (index < 0)
+	if (index < 0){
 		res.status(404).send('User not found');
+	} else {
+		users.splice(index, 1);
+		res.json({"deleted": "ok"});
+	}
+});
 
-	users.splice(index, 1);
+//delete all users
+app.delete("/users/", function(req, res){
+	users = [];
 	res.json({"deleted": "ok"});
 });
 
+//read all users
 app.get("/users", function(req, res){
-	res.json(users);
+	let arUser = users.slice();
+
+	if (!!req.query.limit){
+		let limit = Number(req.query.limit);
+		let offset = Number(req.query.offset) || 0;
+		arUser = users.slice(offset, offset + limit);
+	}
+
+	if (!!req.query.fields){
+		let fields = req.query.fields.split(",");
+		let arResult = [];
+		arUser.forEach((user, index) => {
+			let userCopy = Object.assign({}, user);
+			for (prop in userCopy){
+				if (fields.indexOf(prop) == -1){
+					delete userCopy[prop];
+				}
+			}
+			arResult.push(userCopy);
+		});
+		arUser = arResult;
+	}
+	res.json(arUser);
 });
 
 app.use(function(req, res){
